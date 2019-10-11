8fd0901229774f989444771890bd33d4852005a8
neniputrij <56425716+neniputrij@users.noreply.github.com>
2019-10-11 15:36:21 +0700

    Add files via upload

---
+++ Belajar1.java
@@ -0,0 +1,18 @@
+
+package belajar1;
+
+import java.util.Scanner;
+
+
+public class Belajar1 {
+
+   
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("siapa nama anda : ");
+        String nama = scan.nextLine();
+        System.out.println("nama anda adalah : " + nama);
+        
+    }
+    
+}
